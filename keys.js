585b70715f92de3934b295f1cdc29bbdeda0aa2c
Ryan Villanueva <ryanueva@gmail.com>
2023-02-06 17:44:21 -0500

    trying to get spotify api to run

---
+++ keys.js
@@ -0,0 +1,7 @@
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
+
+//   id: "23676f98c3cb41f287a4201564df3f0d",
+//   secret: "2a53b69286f044f5bd7a741ea844790b"
