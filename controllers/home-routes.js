05173db885fd49be3208e37fd74b2afe4534fe4e
FrancisLao9 <lao.francis@gmail.com>
2023-02-06 22:59:06 -0500

    fixed signup and logout error

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,8 +4,9 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
   try {
-    res.render('homepage', { 
-    });
+    res.render('homepage', {
+      loggedIn: req.session.loggedIn
+     });
   } catch (err) {
     res.status(500).json(err);
   }
