d39fa0ef8f4ce4320bc53afed29a3aff22708995
Melany Baldeon <tochio12910@gmail.com>
2023-02-09 23:10:08 -0500

    got the add button to work

--- seeds/song-seeds.js
+++ seeds/song-seeds.js
@@ -5,28 +5,16 @@ const songData = [
     name: 'Levitating',
     type: 'Pop',
     created_at: new Date(),
-    // comments: [{}, {}],
-    // user: {
-    //     username: 'test_user'
-    // }
   },
   {
     name: 'Save Your Tears',
     type: 'R&B',
     created_at: new Date(),
-    // comments: [{}, {}],
-    // user: {
-    //     username: 'test_user'
-    // }
   },
   {
     name: 'Stay',
     type: 'Slow Pop',
     created_at: new Date(),
-    // comments: [{}, {}],
-    // user: {
-    //     username: 'test_user'
-    // }
   },
  
 ];
--- utils/helpers.js
+++ utils/helpers.js
@@ -14,11 +14,11 @@ module.exports = {
     
         return word;
     },
-    // Removes Diacritical marks from names
+    
     format_deburr: word => {
         return _.deburr(word)
     },
-    // Run API through to test if it is empty (finish function)
+   
     format_isEmpty: (obj) => {
         if (_.isEmpty(obj)) {
             return err
